268152a64f9b6a98d9dc8b73540a4f8c942c628d
KaviPubGee <kpgeeganage@gmail.com>
2026-03-28 18:13:13 +0530

    Sensor Registration & Integrity Validation

--- src/main/java/com/smartcampus/config/SmartCampusApplication.java
+++ src/main/java/com/smartcampus/config/SmartCampusApplication.java
@@ -1,6 +1,8 @@
 package com.smartcampus.config;
 
 import com.smartcampus.api.DiscoveryResource;
+import com.smartcampus.api.RoomResource;
+import com.smartcampus.api.SensorResource;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
@@ -18,6 +20,8 @@ public class SmartCampusApplication extends Application {
         Set<Class<?>> resources = new HashSet<>();
         // Register API resources
         resources.add(DiscoveryResource.class);
+        resources.add(RoomResource.class);
+        resources.add(SensorResource.class);
         return resources;
     }
 }
